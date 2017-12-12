ae18546670ee5a98340c942cd6f85c79e42ed038
John Washburn <jwash@live.unc.edu>
2017-12-11 19:39:40 -0500

    Add files via upload
    This is the working new user php script

--- user.php
+++ user.php
@@ -1,41 +1,41 @@
 <?php
-echo "hello";
-$servername = "classroom.cs.unc.edu";
-$username = "carriems"
-$password = "CH@ngemenow99Please!carriems"
-$database = carriemsdb
 
+$servername = "classroom.cs.unc.edu";
+$username = "carriems";
+$password = "CH@ngemenow99Please!carriems";
+$database = "carriemsdb";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
 if($conn->connect_error){
-	die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
 }
 
-if(isset($_POST['submit'])){
+else {
 
-
-$username = $POST['Username'];
-$password = $POST['Password'];
+$username = $_POST['Username'];
+$password = $_POST['Password'];
 $password = md5($password);
 
-
-
-echo "hello";
-$check_duplicates = mysqli_query($conn, "SELECT 1 FROM F.login WHERE user = '$username'";);  
+$check_duplicates = mysqli_query($conn, "SELECT 1 FROM `F.login` WHERE `user` = '$username'");
 
 if(!$check_duplicates){
-	echo "error".$check_duplicates->error;
-
-}
-if($check_duplicates->num_rows == 0){
-	$result = $mysqli->query("insert into F.login values (NULL, $mysqli->real_escape_string($username),$mysqli->real_escape_string($password))");
-	
+        echo "Error: " . mysqli_error($conn);
 }
 
+else {
 
-
+        if($check_duplicates->num_rows == 0){
+                $insert_new_user = mysqli_query($conn, "INSERT INTO `F.login` VALUES(NULL, '$username', '$password')");
+                if(!$insert_new_user){
+                        echo "Error: " . mysqli_error($conn);
+                }
+        }
 
 }
+
+}
+
+$conn->close();
 
 ?>
