a9a79072536ba1848f89af207983edd1dcad8993
John Washburn <jwash@live.unc.edu>
2017-12-12 01:07:04 -0500

    AJAX now creates users and checks for dups

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-    $("#NewUserForm").on("submit", function(e){
-        e.stopPropagation(); 
-        e.preventDefault(); 
+        $("#NewUserForm").on("submit", function(e){
+        e.stopPropagation();
+        e.preventDefault();
         $.ajax({
-            type: "POST", 
-            url: "user.php", 
-            data: $("#NewUserForm").serialize(), 
+            type: "post",
+            url: "user.php",
+            data: $("#NewUserForm").serialize(),
             error: function(){
-                alert("Error creating user"); 
-            }, 
+                alert("Error creating user. Please try again.");
+            },
             success: function(){
-                alert("User successfully created"); 
+                alert("User successfully created. Please log in with your new credentials.");
             }
-        }); 
-    }); 
+        });
+    });
 })
