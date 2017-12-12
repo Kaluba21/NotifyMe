9474fa50ff6cf3857572535703ebadf9d4e7efc2
John Washburn <jwash@live.unc.edu>
2017-12-12 15:04:45 -0500

    Calendar updated with Cookie checks; login system fully functional

--- calendar.js
+++ calendar.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
     //load up user classes, study groups, calendar, etc from database
     //clear placeholder information and replace with actual user data
-    var newSession = new Session(); 
-}) 
+
+    if(Cookies.get('USER_LOGGEDIN') === undefined){
+        alert("Access Denied. Unauthorized access forbidden.");
+        window.location.href = "https://wwwp.cs.unc.edu/Courses/comp426-f17/users/jwash/final/main.html";
+    }
+    else {
+        var newSession = new Session();
+    }
+
+    var newSession = new Session();
+})
 
 var Session = function(){
 
-    $("#SelectAllBtn").on("click", SelectAllClasses); 
+    $("#SelectAllBtn").on("click", SelectAllClasses);
 
     function SelectAllClasses(){
         var classes = $("#classes").children();
---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+
+function ValidateUser($u, $p){
+        $servername = "classroom.cs.unc.edu";
+        $username = "carriems";
+        $password = "CH@ngemenow99Please!carriems";
+        $database = "carriemsdb";
+
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        $check_user = mysqli_query($conn, "SELECT 1 FROM `F.login` WHERE `user` = '$u' AND `pass` = '$p'");
+
+        if($check_user->num_rows == 0){
+                return false;
+        }
+        else {
+                return true;
+        }
+}
+
+$username = $_POST['Username'];
+$password = $_POST['Password'];
+$password = md5($password);
+
+if(ValidateUser($username, $password)){
+        setcookie("USER_LOGGEDIN", $username, time() + 60, "/");
+        header('Content-type: application/json');
+        print(json_encode(true));
+}
+else {
+         header('HTTP/1.1 401 Unauthorized');
+         header('Content-type: application/json');
+         print(json_encode(false));
+}
+
+?>
